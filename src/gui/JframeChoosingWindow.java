6248931e99ba6d5242430be6b9a43b6ac04abc03
MartinhoCarvalho <martinhocarvalho1987@gmail.com>
2014-11-27 04:43:10 +0000

    JframeChoosingWindow
    fr-1 check
    fr-2 check
    fr-3 ?
    fr-4 this unfinished
    fr-5 check

--- src/gui/JframeChoosingWindow.java
+++ src/gui/JframeChoosingWindow.java
@@ -1,17 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Observable;
 import java.util.Observer;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -20,8 +24,11 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class FrameMain extends JFrame implements Observer {
-    //Atributos
+/**
+ *
+ * @author PTXXI
+ */
+public class JframeChoosingWindow extends JFrame implements Observer {
     private Controller controller;
     private Container mainContainer;
     private JTextField textname;
@@ -30,21 +37,28 @@ public class FrameMain extends JFrame implements Observer {
     private JPanel jpanelWest;
     private JPanel jpaneleast;
     private JPanel jpanelsouth ;
-    private JButton play = null;
-    private JButton change = null;
-    private JButton score = null;
     private JButton exit = null;
-    private String Simbolos [] = {"Novo","User1","User2","User3","User4"}; 
-    private JComboBox jc = null;
-    private JLabel click = null;
-   // private JDesktopPane desk = null;
-      
+    private JButton consonants = null;
+    private JButton accents = null;
+    private JButton hyphen  = null;
+    private JButton Casesensitive  = null;
+    private JButton random = null;
     
-    public FrameMain(Controller controller) {
-        this(controller, 350, 75, 600, 500);
+    
+    //temp
+    private String symbols [] = {"Dificuldade"," 1","2"," 3"," 4"};
+    private String symbols2 [] = {"Nome"," 1","2"," 3"," 4"}; 
+    //temp
+    private JComboBox jc = null;
+    private JComboBox jc2 = null;
+    private JLabel click = null;
+    // private JDesktopPane desk = null;
+   
+    public JframeChoosingWindow(Controller controller){
+               this(controller, 350, 75, 600, 500);
     }
     
-    public FrameMain(Controller controller, int x, int y, int width, int height) {
+    public JframeChoosingWindow(Controller controller, int x, int y, int width, int height) {
         // Cabeçalho
         super("Português do Século XXI");
         this.controller = controller;
@@ -63,7 +77,6 @@ public class FrameMain extends JFrame implements Observer {
         // READY
         controller.notifyObservers();
     }
-    
 
     //############ Methods ###################
     protected void init() {
@@ -84,40 +97,16 @@ public class FrameMain extends JFrame implements Observer {
     
     //JPanelNort
     private void jPanelNorth(){
-        this.jpanel = new JPanel(new FlowLayout());
-        JPanel jp = new JPanel(new GridLayout(2,1));
-        JLabel jb = new JLabel("Português\n ");
-        jb.setFont(jb.getFont().deriveFont(64f));
-        jp.add(jb);
-        //
-        this.jpanel.add(jb);
+        this.jpanel = new JPanel(new GridLayout(3,1));
+       // this.jpanel.setBorder(BorderFactory.createLineBorder(Color.ORANGE, WIDTH));
+        JPanel jp = new JPanel(new GridLayout(1,1));
         
-        JLabel jb2 = new JLabel("do Século XXI\n ");
-        jb2.setFont(jb2.getFont().deriveFont(64f));
-        jp.add(jb2);
-        //
-        this.jpanel.add(jp);
-        //this.jpanel.setBorder(BorderFactory.createLineBorder(Color.ORANGE, WIDTH));
-        //add Atributos
-        this.mainContainer.add(this.jpanel,BorderLayout.NORTH);
-    } 
-    
-    //JPanelCenter
-    private void jPanelCenter(){
-        this.jpanelcenter = new JPanel(new GridLayout(15,1));
-        //this.jpanelcenter.setBorder(BorderFactory.createLineBorder(Color.RED, WIDTH));
-        // this.textname = new JTextField("Perfil do utilizador");
-        
-        this.jc = new JComboBox(this.Simbolos);
-        JPanel jp = new JPanel(new GridLayout(1,6));
-        jp.add(new JLabel(" "));
-        jp.add(new JLabel("Perfil do utilizador"));
-        jp.add(this.jc);
+        jp.add(new JLabel("Perfil atual"));  
+        //jp.add(new JLabel("Nome"));//name player
         
         this.click = new JLabel("Alterar");
-        this.click.setBackground(Color.RED);
-        this.click.addMouseListener(new MouseListener() {
-
+       // this.click.setBackground(Color.RED);
+        this.click.addMouseListener(new MouseListener(){
             @Override
             public void mouseClicked(MouseEvent e) {
                 dialogProfile();
@@ -143,48 +132,70 @@ public class FrameMain extends JFrame implements Observer {
             public void mouseExited(MouseEvent e) {
                 //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
-            
-            
-            
-            
         });
-        
         jp.add(this.click);
+        
+        jp.add(new JLabel(" "));
+        JLabel jb = new JLabel("        Português");
+        jb.setFont(jb.getFont().deriveFont(64f));
+ 
+        this.jpanel.add(jp);
+        this.jpanel.add(jb);
+        //this.jpanel.add(new JLabel(" ")); 
+        JLabel jb2 = new JLabel("     do Século XXI");
+        jb2.setFont(jb2.getFont().deriveFont(64f));
+     
+        this.jpanel.add(jb2);
+     
+        this.mainContainer.add(this.jpanel,BorderLayout.NORTH);
+    } 
+    
+    //JPanelCenter
+    private void jPanelCenter(){
+        this.jpanelcenter = new JPanel(new GridLayout(10,1));
+   
+        this.jc = new JComboBox(this.symbols);
+        JPanel jp = new JPanel(new GridLayout(1,6));
+        jp.add(new JLabel(" "));
+        jp.add(new JLabel("     Dificuldade"));
+        jp.add(this.jc);
+        jp.add(new JLabel(" ")); 
+       
         this.jpanelcenter.add(jp);
        
         this.jpanelcenter.add(new JLabel(" "));
         JPanel jp1 = new JPanel(new GridLayout(1,6));
         jp1.add(new JLabel(" "));
+        this.consonants = new JButton("Consoantes");
+        jp1.add(this.consonants);
         jp1.add(new JLabel(" "));
-        this.play  = new JButton("Jogar");
-        jp1.add(this.play,BorderLayout.CENTER);
+        this.accents = new JButton("Acentos");
+        jp1.add(this.accents);
         jp1.add(new JLabel(" "));
-        jp1.add(new JLabel(" "));
-        
         this.jpanelcenter.add(jp1);
         
-        JPanel jp2 = new JPanel(new GridLayout(1,6));
+        JPanel jp2 = new JPanel(new GridLayout(1,3));
         jp2.add(new JLabel(" "));
+        this.hyphen  = new JButton("Hífen");
+        jp2.add(this.hyphen,BorderLayout.CENTER);
         jp2.add(new JLabel(" "));
-        this.score  = new JButton("Pontuacoes");
-        jp2.add(this.score,BorderLayout.CENTER);
+        this.Casesensitive = new JButton("Maiúsculas E Minúsculas");
+        jp2.add(this.Casesensitive);
         jp2.add(new JLabel(" "));
-        jp2.add(new JLabel(" "));
-        
         this.jpanelcenter.add(new JLabel(" "));
         this.jpanelcenter.add(jp2);
-        
+//        
         JPanel jp3 = new JPanel(new GridLayout(1,6));
         jp3.add(new JLabel(" "));
         jp3.add(new JLabel(" "));
-        this.exit  = new JButton("Sair");
-        jp3.add(this.exit,BorderLayout.CENTER);
+        this.random = new JButton("Aleatorio");
+        jp3.add(this.random,BorderLayout.CENTER);
         jp3.add(new JLabel(" "));
         jp3.add(new JLabel(" "));
         
         this.jpanelcenter.add(new JLabel(" "));
         this.jpanelcenter.add(jp3);
-       
+//       
         //centro
         this.mainContainer.add(this.jpanelcenter,BorderLayout.CENTER);
     } 
@@ -202,50 +213,79 @@ public class FrameMain extends JFrame implements Observer {
     private void jPanelSouth(){
         //south
         this.jpanelsouth = new JPanel(new FlowLayout());
-         this.mainContainer.add(this.jpanelsouth,BorderLayout.SOUTH);
+        this.mainContainer.add(this.jpanelsouth,BorderLayout.SOUTH);
     }
     
     //JPanelWest
     private void jPanelWest(){
         //weast 
-        this.jpanelWest = new JPanel(new FlowLayout());
-        this.mainContainer.add(this.jpanelWest,BorderLayout.WEST);
-    }
-
-    //JPanelEast
-    private void jPanelEast(){
-         //East
-        this.jpaneleast = new JPanel(new FlowLayout());
-        this.mainContainer.add(this.jpaneleast,BorderLayout.EAST);
-    }
-    
-    
-    private void registerListeners() {
-        //Exit
-        this.exit.addActionListener(new ActionListener() {
+        this.jpanelWest = new JPanel(new GridLayout(10,1));
+        this.jpanelWest.add(new JLabel("      "));
+        this.jpanelWest.add(new JLabel("      "));
+        this.jpanelWest.add(new JLabel("      "));
+        this.jpanelWest.add(new JLabel("      "));
+        this.jpanelWest.add(new JLabel("      "));
+        this.jpanelWest.add(new JLabel("      "));
+        this.jpanelWest.add(new JLabel("      "));
+        this.jpanelWest.add(new JLabel("      "));
+        this.exit = new JButton("");
+        JLabel voltar = new JLabel("Voltar");
+        this.jpanelWest.add(voltar);
+        this.click = new JLabel("Alterar");
+      
+        voltar.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //insertCode
+            }
 
             @Override
-            public void actionPerformed(ActionEvent e) {
-                //System.gc();
-                System.exit(0);
+            public void mousePressed(MouseEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+               // dialogProfile();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
         });
        
         
+        this.mainContainer.add(this.jpanelWest,BorderLayout.WEST);
+    }
+    //JPanelEast
+    private void jPanelEast(){
+         //East
+        this.jpaneleast = new JPanel(new FlowLayout());
+       
+        this.mainContainer.add(this.jpaneleast,BorderLayout.EAST);
     }
     
-  
+    private void registerListeners() {
+        //Exit
+    }
+    
     //Create Profile
     private void dialogProfile(){
         JPanel jp = new JPanel(new GridLayout(2,6));
         // jp.add(new JLabel(" "));
-        jp.add(new JLabel("           Nome"));
+        jp.add(new JLabel("Nome"));
         
-        
-       JTextField tx = new JTextField();
-       jp.add(tx); 
-       jp.add(new JLabel(" "));
-       String options[] = {"Cancelar","Gravar"};
+      
+       this.jc2 = new JComboBox(this.symbols2);
+       jp.add(this.jc2);
+       String options[] = {"Alterar","Cancelar"};
        int value = JOptionPane.showOptionDialog(null, jp, "Novo Perfil de Utilizador", JOptionPane.NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options , options[0]);
        
         System.out.println(""+value);
@@ -256,7 +296,6 @@ public class FrameMain extends JFrame implements Observer {
        }
        
     }
-    
     
     @Override
     public void update(Observable t, Object o) {
